ab3a1cd661d26ab568eabbbd8ba13091f7c1c989
Lena Zimin <Bogypesn@gmail.com>
2024-02-13 15:45:55 -0500

    uploaded new project

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int count = 2 + 2;
+        System.out.println(count);
+    }
+}
